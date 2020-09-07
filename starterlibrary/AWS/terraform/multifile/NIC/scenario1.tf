e4e48e7d6b81a792cf1dc2fa4fac81a29054a21c
Masaomi MORI <masaomi_mori@niandc.co.jp>
2020-09-07 03:29:16 +0000

    2020/09/07-A4 Signed-off-by: Masaomi MORI <masaomi_mori@niandc.co.jp>

--- starterlibrary/AWS/terraform/multifile/NIC/scenario1.tf
+++ starterlibrary/AWS/terraform/multifile/NIC/scenario1.tf
@@ -77,20 +77,20 @@ resource "aws_alb_target_group_attachment" "alb" {
 
 resource "aws_alb_listener" "alb" {
   load_balancer_arn = "${aws_alb.alb.arn}"
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "${aws_lb_listener_certificate.alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08"
+#  certificate_arn   = "${aws_lb_listener_certificate.alb.arn}"
   default_action {
     type            = "forward"
     target_group_arn = "aws_alb_target_group.alb.arn"
   }
 }
 
-resource "aws_lb_listener_certificate" "alb" {
-  listener_arn    = aws_lb_listener.front_end.arn
-  certificate_arn = aws_acm_certificate.example.arn
-}
+#resource "aws_lb_listener_certificate" "alb" {
+#  listener_arn    = aws_lb_listener.front_end.arn
+#  certificate_arn = aws_acm_certificate.example.arn
+#}
 
 resource "aws_ebs_volume" "volume_webserver" {
     availability_zone = "${var.availability_zone}"
